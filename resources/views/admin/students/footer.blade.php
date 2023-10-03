aed85c812f712cd77baf7ce1547cabb00660b622
imyourbug <duongvankhai2022001@gmail.com>
2023-10-03 20:41:08 +0700

    first commit

---
+++ resources/views/admin/students/footer.blade.php
@@ -0,0 +1,71 @@
+<div class="done">
+    <br>
+    <div class="address">
+        <address style="font-size: 20px; font-weight: bold;">
+            Trường Đại học Nguyễn Tất Thành
+        </address>
+        <br>
+        <div style="display: flex;">
+            <div class="blockaddress">
+                <address>
+                    <span style="font-size:20px;font-weight:bold">THÔNG TIN LIÊN HỆ</span>
+                    <br>
+                    <ul>
+                        <li>Cơ sở chính: 300A – Nguyễn Tất Thành, Phường 13, Quận 4, TP. Hồ Chí Minh, Việt Nam</li>
+                        <li>Điện thoại: 1900 2039</li>
+                        <li>Email: tuyensinh@ntt.edu.vn</li>
+                    </ul>
+                </address>
+            </div>
+            <div class="blockaddress">
+                <address>
+                    <span style="font-size:20px;font-weight:bold">GIỚI THIỆU</span>
+                    <br>
+                    <ul>
+                        <li>Thư ngỏ</li>
+                        <li>Tầm nhìn - Sứ mạng</li>
+                        <li>Văn hóa ĐH Nguyễn Tất Thành</li>
+                        <li>Thông tin 3 công khai</li>
+                        <li>Đảm bảo chất lượng</li>
+                        <li>Phát triển bền vững</li>
+                        <li>Trường Trung cấp Nguyễn Tất Thành</li>
+                        <li>Tạp chí Khoa học - Đại học Nguyễn Tất Thành</li>
+                        <li>Trường Tiểu học Anh Việt Mỹ</li>
+                        <li>Liên hệ</li>
+                    </ul>
+                </address>
+            </div>
+            <div class="blockaddress">
+                <address>
+                    <span style="font-size:20px;font-weight:bold">VIỆN - TRUNG TÂM</span>
+                    <br>
+                    <ul>
+                        <li>Viện Đào Tạo Quốc Tế</li>
+                        <li>Viện Công Nghệ Cao</li>
+                        <li>Viện Đào tạo Sau đại học</li>
+                        <li>Viện E-learning</li>
+                        <li>TT Thông tin Thư viện</li>
+                        <li>TT Ngoại ngữ NTT</li>
+                        <li>TT Tin học NTT</li>
+                        <li>Phòng Quan hệ Doanh nghiệp và Việc làm Sinh viên</li>
+                        <li>TT Đào tạo theo nhu cầu Xã hội</li>
+                        <li>TT Xuất khẩu lao động Texgamex</li>
+                    </ul>
+                </address>
+            </div>
+            <div class="blockaddress">
+                <address>
+                    <span style="font-size:20px;font-weight:bold">PHÒNG BAN</span>
+                    <br>
+                    <ul>
+                        <li>Công đoàn Trường ĐH Nguyễn Tất Thành</li>
+                        <li>Phòng Công tác Sinh viên</li>
+                        <li>Phòng Quản lý Đào tạo</li>
+                        <li>TT Sáng tạo & Ươm mầm khởi nghiệp</li>
+                        <li>Đoàn Thanh niên - Hội Sinh viên trường Đại học Nguyễn Tất Thành</li>
+                    </ul>
+                </address>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/admin/students/header.blade.php
@@ -0,0 +1,53 @@
+<div class="block-header">
+    <div class="block-1">
+        <div class="block-1-image">
+            <a href="{{ route('home') }}"><img src="/template/admin/image/logo.svg" height="60px"
+                    alt="Smiley face"></a>&ensp;
+        </div>
+        <div class="block-1-right">
+                <div class="search-bar">
+                    <input class="text-search" value="{{ request()->input('content-search') }}"
+                        placeholder="Nhập tìm kiếm" name="content-search">
+                    <button class="btn-search" ><i class="fas fa-search"></i></button>
+                </div>
+            <div class="in_right">
+                @if (Session::get('user') !== null)
+                    @if (Session::get('user')->roles == 1)
+                        <a href="{{ route('admin.home') }}"><i class="fas fa-user-circle"></i>
+                            {{ Session::get('user')->name }}
+                        </a>&ensp;
+                    @else
+                        <a href="{{ route('changepass') }}"><i class="fas fa-user-circle"></i>
+                            {{ Session::get('user')->name }}
+                        </a>&ensp;
+                    @endif
+                @else
+                    <a href="{{ route('login') }}"><i class="fas fa-user-circle"></i> Tài khoản
+                    </a>&ensp;
+                @endif&ensp;
+                <a href="{{ route('logout') }}" onclick="return confirm('Bạn có muốn đăng xuất?')"><i
+                        class="fas fa-sign-out-alt"></i> Đăng xuất</a>
+            </div>
+        </div>
+    </div>
+    <div class="menu">
+        <ul>
+            <li class="in-menu" style="width: 140px"><a href="{{ route('home') }}"><i
+                        class="nav-icon fas fa-bars"></i>
+                    Trang chủ</a></li>
+            <li class="in-menu" style="width: 140px"><a href="{{ route('regis-subject') }}">
+                    Đăng ký tín chỉ</a></li>
+            <li class="in-menu" style="width: 140px"><a href="#">
+                    Học phí</a>
+                <ul class="sub-menu">
+                    <li class="in-menu"><a href="{{ route('tuition') }}">
+                            Tra cứu học phí</a></li>
+                    <li class="in-menu"><a href="{{ route('tuition.payment') }}">
+                            Nộp học phí</a></li>
+                </ul>
+            </li>
+            <li class="in-menu" style="width: 170px"><a href="{{ route('information') }}">
+                    Thông tin cá nhân</a></li>
+        </ul>
+    </div>
+</div>
